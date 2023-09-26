7d98071557f90575a3bbe23b7afbdd0fa92f0b38
Zachary Boehm <zboehm@menard-inc.com>
2023-09-26 11:38:46 -0500

    Back to static generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,6 @@ import nodejs from '@astrojs/node';
 export default defineConfig({
 	site: 'https://github.com/Tired-Fox',
     base: '/Totally-Not-a-Bunch-of-Cats',
-    output: 'hybrid',
-    adapter: nodejs({
-        mode: 'middleware'
-    }),
 	integrations: [mdx(), sitemap(), tailwind(), solid()],
     markdown: {
         remarkPlugins: [],
